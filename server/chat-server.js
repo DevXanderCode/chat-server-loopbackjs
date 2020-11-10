668d26fd1fd703476dec6973ca40d599b6254928
Alex Nweke <nwekealexchinedu@gmail.com>
2020-11-10 22:08:12 +0100

    initialize the websock and on connection console log the message and also change the port to 8080

--- server/chat-server.js
+++ server/chat-server.js
@@ -1,2 +1,8 @@
 const WebSocket = require('ws');
 var models = require('./server').models;
+
+const ws = new WebSocket.Server({ port: 8080 });
+
+ws.on('connection', (message) => {
+	console.log('Got Message', message);
+});
