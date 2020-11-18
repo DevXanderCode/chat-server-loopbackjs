2a9229dfb34d6eb27b40eed1565475fe821bd0fd
Alex Nweke <nwekealexchinedu@gmail.com>
2020-11-18 11:12:21 +0100

    started creating the signup logic

--- server/chat-server.js
+++ server/chat-server.js
@@ -6,5 +6,46 @@ const ws = new WebSocket.Server({ port: 8080 });
 ws.on('connection', (ws) => {
 	ws.on('message', (message) => {
 		console.log('Got Message', JSON.parse(message));
+
+		let parsed = JSON.parse(message);
+
+		if (parsed) {
+			switch (parsed.type) {
+				case 'SIGNUP':
+					models.User.create(parsed.data, (err, user) => {
+						if (err) {
+							ws.send(
+								JSON.stringify({
+									type: 'ERROR',
+									error: err
+								})
+							);
+						} else {
+							models.Profile.create(
+								{
+									userId: user.id,
+									name: parsed.data.name,
+									email: parsed.data.email,
+									username: parsed.data.username,
+									password: parsed.data.password
+								},
+								(profileError, profile) => {
+									if (profileError) {
+										ws.send(
+											JSON.stringify({
+												type: 'error',
+												error: profileError
+											})
+										);
+									}
+								}
+							);
+						}
+					});
+
+				default:
+					console.log('Nothing To See Here');
+			}
+		}
 	});
 });
